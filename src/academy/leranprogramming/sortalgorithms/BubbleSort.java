1a2d8fa5d5a79395bfe0e30f3c1f09e916948d29
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2019-05-27 07:50:23 -0300

    Criado a classe SelectionSort esse exemplo mostra outra forma de ordenar arrays.

--- src/academy/leranprogramming/sortalgorithms/BubbleSort.java
+++ src/academy/leranprogramming/sortalgorithms/BubbleSort.java
@@ -4,6 +4,20 @@ public class BubbleSort {
 	
 	public static void main(String[] args) {
 		
+		int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+		
+		for(int lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+			for(int i = 0; i< lastUnsortedIndex; i++) {
+				if(intArray[i] > intArray[i + 1]) {
+					swap(intArray, i, i + 1);
+				}
+			}
+		}
+		
+		for(int i = 0; i < intArray.length; i++) {
+			System.out.println(intArray[i]);
+		}
+				
 		
 	}
 	
